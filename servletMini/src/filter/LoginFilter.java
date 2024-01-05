47c21fe0db9b0dbd51e2dd90eb5544fda85360b9
HyeJJiny <pizza.jinzza@gmail.com>
2024-01-05 11:28:13 +0900

    login 기능 완성

---
+++ servletMini/src/filter/LoginFilter.java
@@ -0,0 +1,36 @@
+package filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebFilter(urlPatterns = {"*.do"})
+public class LoginFilter implements Filter {
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		
+		HttpServletRequest httpRequest = (HttpServletRequest) request;
+		HttpServletResponse httpResponse = (HttpServletResponse) response;
+		
+		HttpSession session = httpRequest.getSession(false); 	// 매핑된 세션객체를 리턴하는데 없으면 새로 생성해서 리턴. false 쓰면 세션을 새로 생성하지 X
+		
+		String path = httpRequest.getRequestURI();
+		System.out.println(path);
+		
+		if(!path.contains("/login.do")) {
+			if(session == null || session.getAttribute("userId") == null) {
+				httpResponse.sendRedirect("login.jsp");
+				return;
+			} 
+		}
+		chain.doFilter(request, response);
+	}
+
+}
---
+++ servletMini/src/login/controller/LoginController.java
@@ -0,0 +1,60 @@
+package login.controller;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import user.dao.UserDAO;
+import user.dto.UserDTO;
+
+@WebServlet("/login.do")
+public class LoginController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String userId = request.getParameter("userId");
+		String pw = request.getParameter("password");
+		UserDTO user = null;
+		
+		String url = "login.errors.jsp";
+		
+		if(userId == null || userId == "" ) {
+			request.setAttribute("error", "아이디를 재확인해주세요");
+			request.getRequestDispatcher(url).forward(request, response);
+		}else if (pw == null || pw == "") {
+			request.setAttribute("error", "비밀번호를 재확인해주세요");
+			request.getRequestDispatcher(url).forward(request, response);
+		} else {
+		try {
+			System.out.println(userId);
+			user = UserDAO.getUserByUserIdandPassword(userId, pw);
+			System.out.println(user);
+			
+			if (user == null) {
+				request.setAttribute("error", "아이디와 비밀번호가 맞지 않습니다.");
+				request.getRequestDispatcher(url).forward(request, response);
+			} else {
+				
+				HttpSession session = request.getSession();
+				session.setAttribute("userId", user.getUserId());
+				session.setAttribute("password", user.getPassword());
+				
+				url = "main.jsp";
+				response.sendRedirect(url);
+				
+			}
+				return;
+			}
+		
+		catch (SQLException e) {
+			request.setAttribute("error", "존재하지 않는 아이디입니다.");
+			request.getRequestDispatcher(url).forward(request, response);
+		}
+		}
+	}
+}
